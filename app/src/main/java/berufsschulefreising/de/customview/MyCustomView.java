7ba7f800f007194e628baac3f9be6be5cf4a2480
Erich Schmid <JavaAlberto@freenet.de>
2016-11-29 00:56:51 +0100

    first commit

--- app/src/main/java/berufsschulefreising/de/customview/MyCustomView.java
+++ app/src/main/java/berufsschulefreising/de/customview/MyCustomView.java
@@ -3,17 +3,31 @@ package berufsschulefreising.de.customview;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Albrecht on 28.11.2016.
  */
 
-public class MyCustomView extends View
-{
-    public MyCustomView(Context context, AttributeSet attrs)
-    {
+public class MyCustomView extends RelativeLayout {
+
+    View rootView;
+    TextView valueTextView;
+    View minusButton;
+    View plusButton;
+
+    public MyCustomView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
+    private void init(Context context) {
+        //do setup work here
     }
-
+}
