996b6293697771e77d69fd977e1e7cf157b66d57
Erich Schmid <JavaAlberto@freenet.de>
2016-11-28 11:51:28 +0100

    Initial commit

---
+++ app/src/main/java/berufsschulefreising/de/customview/MyCustomView.java
@@ -0,0 +1,19 @@
+package berufsschulefreising.de.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Albrecht on 28.11.2016.
+ */
+
+public class MyCustomView extends View
+{
+    public MyCustomView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    }
+
